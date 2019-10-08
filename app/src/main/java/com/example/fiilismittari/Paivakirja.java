02c34fe5f279c258d85461bceced67921b5b07a5
GGeru <kerttu.kautto@gmail.com>
2019-10-08 15:15:01 +0300

    Merge remote-tracking branch 'origin/master'
                                        # Conflicts:
                                        #	app/src/main/AndroidManifest.xml

--- app/src/main/java/com/example/fiilismittari/Paivakirja.java
+++ app/src/main/java/com/example/fiilismittari/Paivakirja.java
@@ -2,24 +2,32 @@ package com.example.fiilismittari;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
+import java.io.IOException;
 import java.text.DateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 
 //this is an activity
 
 public class Paivakirja extends AppCompatActivity {
+
     RadioGroup radioGroup;
     int chosenRadioId;
     Intent profileIntent;
     int savedMood;
     String currentDate;
+    TextView testi;
+    ArrayList<DataPoint> dataPoints;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,11 +36,13 @@ public class Paivakirja extends AppCompatActivity {
 
         Intent paivakirjaIntent = getIntent(); //get the intent that started this activity
         chosenRadioId = paivakirjaIntent.getIntExtra(MainActivity.CHECKED_BUTTON, 0); //the chosen radiobutton id in the main activity
-        TextView testi = findViewById(R.id.textView2); // just some test code to see if the radiobutton id comes through
+        testi = findViewById(R.id.textView2); // just some test code to see if the radiobutton id comes through
 
         Calendar calendar = Calendar.getInstance(); //https://www.youtube.com/watch?v=Le47R9H3qow
         currentDate = DateFormat.getDateInstance().format(calendar.getTime());
-        testi.setText(currentDate);
+//        testi.setText(currentDate);
+        saveMood();
+        getAverageMood();
     }
 
     public void onTestButtonClick(View v) { //this button takes you to profile activity
@@ -40,23 +50,54 @@ public class Paivakirja extends AppCompatActivity {
         startActivity(profileIntent);
     }
 
-    private void saveMood() { //kesken!
+    public void saveMood() { //put the chosen mood in the globalmodel for saving
         switch (chosenRadioId) {
             case R.id.badMood:
-                GlobalModel.getInstance().getMoods().add(new DataPoint(1, currentDate)); //GlobalModel is a  singleton, check lecture 6 
+                GlobalModel.getInstance().getDataPoints().add(new DataPoint(1, currentDate)); //GlobalModel is a  singleton, check lecture 6
                 break;
             case R.id.notGoodMood:
-                GlobalModel.getInstance().getMoods().add(new DataPoint(2, currentDate));
+                GlobalModel.getInstance().getDataPoints().add(new DataPoint(2, currentDate));
                 break;
             case R.id.okMood:
-                GlobalModel.getInstance().getMoods().add(new DataPoint(3, currentDate));
+                GlobalModel.getInstance().getDataPoints().add(new DataPoint(3, currentDate));
                 break;
             case R.id.niceMood:
-                GlobalModel.getInstance().getMoods().add(new DataPoint(4, currentDate));
+                GlobalModel.getInstance().getDataPoints().add(new DataPoint(4, currentDate));
                 break;
             case R.id.greatMood:
-                GlobalModel.getInstance().getMoods().add(new DataPoint(5, currentDate));
+                GlobalModel.getInstance().getDataPoints().add(new DataPoint(5, currentDate));
                 break;
         }
+    }
+
+    public void getAverageMood() { //count the average mood from the datapoints collected
+        dataPoints = GlobalModel.getInstance().getDataPoints(); //get the list of datapoints from GlobalModel
+        double moodSum = 0;
+        int i;
+        for (i = 0; i < dataPoints.size(); i++) {
+            double thisMood = (double) dataPoints.get(i).getMood();
+            moodSum += thisMood;
+        }
+
+        double average = (double) moodSum / dataPoints.size();
+        testi.setText(Double.toString(average));
+
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //make shared preferences, as in make a place where we save stuff
+        SharedPreferences prefPut = getSharedPreferences( "MyTestPref", Activity.MODE_PRIVATE);
+        //i have no friking idea
+        SharedPreferences.Editor prefEditor = prefPut.edit();
+        //objectserializer is a class that separates things in the dataPoints arraylist
+        try {
+            prefEditor.putString("moods", ObjectSerializer.serialize(dataPoints));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
