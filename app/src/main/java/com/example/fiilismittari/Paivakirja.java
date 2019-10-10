fa1f156624012b10d54a94c87c786a5b84dfdca4
Onicki <anne.nyberg89@gmail.com>
2019-10-10 09:03:59 +0300

    9.10.2019 20:54

--- app/src/main/java/com/example/fiilismittari/Paivakirja.java
+++ app/src/main/java/com/example/fiilismittari/Paivakirja.java
@@ -160,6 +160,8 @@ public class Paivakirja extends AppCompatActivity {
         Log.d("Fiilismittari", "jee loaddata toimi");
     }
 
+    //https://www.youtube.com/watch?v=hHjFIG0TtA0 were reference to calendar
+
 
 }
 
